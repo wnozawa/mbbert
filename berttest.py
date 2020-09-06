cce940d6ea7dc908576826d5c5a6810f2c52f8b2
Wataru Nozawa <wnwnwnwnwnwn@gmail.com>
2020-09-06 15:53:05 +0900

    edit berttest.py

--- berttest.py
+++ berttest.py
@@ -1,43 +1,32 @@
-# load model
-# import sentence_transformers
 from sentence_transformers import SentenceTransformer
-# from ..sentencetransformers.sentence_transformers
-# あとでパス直す
-import numpy as np
 
+# load BERT model
 model_path = "training_bert_japanese"
 model = SentenceTransformer(model_path)
 
+# load Q&A
 import pandas
 df_header = pandas.read_csv('AllQA.csv')
 dg = df_header.iloc[:,0]
 
 
 questions = dg.astype(str).tolist() # 文字列のリストに変換
-# plain = '\n'.join(dglst)         # 空白文字で結合
 
-# print(plain)
+# vectorize questions
 question_vectors = model.encode(questions)
 
+# save question vectors
 import pickle
 question_vectors_pickle_name="question_vectors.pickle"
 f = open(question_vectors_pickle_name,'wb')
 pickle.dump(question_vectors,f)
 f.close
 
-import pprint
 
-# pprint.pprint(question_vectors)
-
-# input を取って
-# print('質問をどうぞ')
 query = ['赤字でも売れますか？']
 # input to vector by sentenceBERT
 query_vector = model.encode(query)
-# get similarities
 
-# get best 10 questions
-# pprint.pprint(query_vector)
 
 import scipy
 
